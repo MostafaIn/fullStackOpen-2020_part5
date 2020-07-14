9d81d08f36b9bc14ebeec7672d33df97374c2269
mostafaIn <mostafa2015se@gmail.com>
2020-07-14 16:50:05 +0300

    5.2: bloglist frontend, step2

--- src/App.js
+++ src/App.js
@@ -17,14 +17,24 @@ const App = () => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if(loggedUserJSON){
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
   }, [])
+
 
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
       const user = await loginService.login({
         username, password
-      }) 
+      })
+
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
+ 
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,6 +42,11 @@ const App = () => {
       setErrMsg('Wrong credentials!')
       setTimeout(() => setErrMsg(null), 5000)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
   }
 
   console.log(user)
@@ -67,7 +82,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in</p>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
