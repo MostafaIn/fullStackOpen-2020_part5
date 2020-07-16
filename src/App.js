95fe4f996a1541ec4267741cdecf5ebab019e13a
mostafaIn <mostafa2015se@gmail.com>
2020-07-16 13:31:52 +0300

    5.9*: Blog list frontend, step9

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs( blogs.sort((a,b) => b.likes - a.likes) )
     )  
 
     const loggedUserJSON = window.localStorage.getItem('loggedUser')
