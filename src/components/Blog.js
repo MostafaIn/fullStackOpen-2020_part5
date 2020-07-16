5d38843a0dd13d13a3d639846fde1d362b13e896
mostafaIn <mostafa2015se@gmail.com>
2020-07-16 09:15:00 +0300

    5.7* Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React,{ useState } from 'react'
+
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+    {blog.title} {blog.author} 
+    <button onClick={toggleVisibility}> {!visible ? 'view' : 'hide'} </button>
+    {visible &&
+      <div>
+        <p>{blog.url}</p>
+        <p>{blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </div>
+    }
+    </div>
+  )
+}
+
 
 export default Blog
