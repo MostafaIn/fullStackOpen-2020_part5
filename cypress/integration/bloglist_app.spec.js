54b3dff70a5a89179cb19af5535153a3d92dc539
mostafaIn <mostafa2015se@gmail.com>
2020-07-21 23:52:20 +0300

    5.18: bloglist end to end testing, step2

--- cypress/integration/bloglist_app.spec.js
+++ cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,13 @@
 describe('Blog App', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      name:'Mostafa Hazareh',
+      username:'MosHaz',
+      password:'123456'
+    }
+    cy.request('POST', 'http://localhost:3000/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,6 +15,25 @@ describe('Blog App', function() {
     cy.contains('log in to application')
     cy.get('#username')
     cy.get('#password')
-    cy.contains('login').click()
+    cy.contains('login')
+  })
+
+  describe('LOG IN', function() {
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('invalid')
+      cy.get('#password').type('wrong')
+      cy.contains('login').click()
+      cy.get('.error')
+        .should('contain','wrong username or password!')
+        .and('have.css', 'color' , 'rgb(139, 0, 0)')
+    })
+
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('MosHaz')
+      cy.get('#password').type('123456')
+      cy.contains('login').click()
+      cy.contains('Mostafa Hazareh logged in')
+
+    })
   })
 })
